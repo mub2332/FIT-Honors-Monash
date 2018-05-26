917b9e175511269d4f9a46d0db43fdfb433af9a0
mmah65 <mmah65@student.monash.edu>
2018-05-26 21:43:47 +1000

    Removed redundant public modifier in interface method

--- code/src/main/java/com/fit3170/allocation/repositories/ProjectRepository.java
+++ code/src/main/java/com/fit3170/allocation/repositories/ProjectRepository.java
@@ -7,5 +7,5 @@ import java.util.List;
 
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
-    public List<Project> findProjectsByDescriptionOrTitleContainingIgnoreCase(String s);
+    List<Project> findProjectsByDescriptionOrTitleContainingIgnoreCase(String s);
 }
