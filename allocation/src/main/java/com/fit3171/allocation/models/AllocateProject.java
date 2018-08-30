3c2cdd82f8ade5f235dfe53606bb1bb9584aaa34
zayan <azikr1@student.monash.edu>
2018-08-30 11:33:28 +0530

    allocateproject logic

--- allocation/src/main/java/com/fit3171/allocation/models/AllocateProject.java
+++ allocation/src/main/java/com/fit3171/allocation/models/AllocateProject.java
@@ -15,14 +15,17 @@ public class AllocateProject {
     @RequestMapping(value = "/allocate", method = RequestMethod.POST)
     public String allocateProject(@RequestParam("studentID") Long studentID, @RequestParam("ProjectID") Long ID) {
         StudentService studentservice = new StudentService();
-        Student student = studentservice.findByID(studentID).get();
+        Student student = studentservice.findById(studentID).get();
 
 
         ProjectService projectService = new ProjectService();
         Project project = projectService.findById(ID).get();
         student.allocateProject(project.getId());
+        student.removePreference(project);
+
         return null;
 
-    }*/
+    }
+    */
 
 }
